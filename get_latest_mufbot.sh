a0b2b06610cd58302ddb08d342288945086e677a
Simon <hello@gamersi.at>
2024-09-22 15:34:26 +0200

    update script

--- get_latest_mufbot.sh
+++ get_latest_mufbot.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
-# get the latest release from the MUFbot repository
-# then install it here
-
 GREEN='\033[0;32m'
 NC='\033[0m'
 RED='\033[0;31m'
 
-sp="/-\|"
-sc=0
 
 spin() {
-    printf "\b${sp:sc++:1}"
-    ((sc==${#sp})) && sc=0
-}
-
-startspin() {
-    printf " "
-    while true; do
-        spin
-        sleep 0.1
+    local pid=$1
+    local delay=0.1
+    local spinstr='|/-\'
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
     done
+    printf "    \b\b\b\b"
 }
 
-endspin() {
-    printf "\r%s\n" "$@"
+run_with_spinner() {
+    local command="$1"
+    local message="$2"
+    echo -n "$message"
+    eval "$command" &
+    spin $!
+    wait $!
+    echo -e "$GREEN DONE$NC"
 }
 
 # check if the script is run as root
@@ -39,61 +41,31 @@ SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME.service"
 URL="https://api.github.com/repos/muffonlabs/mufbot-dc/releases/latest"
 LATEST=$(curl -s $URL | grep "browser_download_url" | cut -d '"' -f 4)
 
-echo -n "$GREEN[+] Downloading the latest release of MUFbot...$NC"
-startspin
-wget $latest
-chmod +x mufbot-dc
-endspin "$GREEN[+] Download complete$NC"
-
+run_with_spinner "wget $LATEST && chmod +x mufbot-dc" "$GREEN[+] Downloading the latest release of mufbot...$NC"
 
 # stop the service
-echo -n "$GREEN[+] Stopping the service...$NC"
-startspin
 if [ -f $SERVICE_FILE ]; then
-    systemctl stop $SERVICE_NAME
-    rm $SERVICE_FILE
+    run_with_spinner "systemctl stop $SERVICE_NAME && rm $SERVICE_FILE" "$GREEN[+] Stopping the service...$NC"
 fi
-endspin "$GREEN[+] Service stopped$NC"
 
 # create start script
-echo -n "$GREEN[+] Creating the start script...$NC"
-startspin
-if [ -f $START_SCRIPT ]; then
-    rm $START_SCRIPT
-fi
-
-mkdir -p /root/.scripts
-
-echo "#!/bin/bash
+run_with_spinner "mkdir -p /root/.scripts && echo '#!/bin/bash
 cd ~/mufbot-dc
 chmod +x mufbot-dc
-./mufbot-dc" > $START_SCRIPT
-chmod +x $START_SCRIPT
-
-endspin "$GREEN[+] Start script created$NC"
+./mufbot-dc' > $START_SCRIPT && chmod +x $START_SCRIPT" "$GREEN[+] Creating the start script...$NC"
 
 # create the service file
-echo -n "$GREEN[+] Creating the service file...$NC"
-startspin
-
-echo "[Unit]
-Description=MUFbot Discord Client
+run_with_spinner "echo '[Unit]
+Description=mufbot Discord Client
 After=multi-user.target
-
 [Service]
 Type=simple
 ExecStart=/bin/bash $START_SCRIPT
 Restart=on-failure
-
 [Install]
-WantedBy=multi-user.target" > $SERVICE_FILE
-
-endspin "$GREEN[+] Service file created$NC"
+WantedBy=multi-user.target' > $SERVICE_FILE" "$GREEN[+] Creating the service file...$NC"
 
 # start the service
-echo -n "$GREEN[+] Starting the service...$NC"
-startspin
-systemctl daemon-reload
-systemctl enable $SERVICE_NAME
-systemctl start $SERVICE_NAME
-endspin "$GREEN[+] Service started$NC"
+run_with_spinner "systemctl daemon-reload && systemctl enable $SERVICE_NAME && systemctl start $SERVICE_NAME" "$GREEN[+] Starting the service...$NC"
+
+echo -e "${GREEN}mufbot has been installed successfully!${NC}"
