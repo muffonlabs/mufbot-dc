2209db01936e6d5fb5031157eee992bba995505f
xyloflake <xyloflake@gmail.com>
2024-10-07 02:16:24 +0530

    add custom build script to warn dev when not using sccache

---
+++ build.rs
@@ -0,0 +1,13 @@
+use std::env;
+
+fn main() {
+
+    // warn dev if not using sccache
+    let rustc_wrapper = env::var("RUSTC_WRAPPER").unwrap_or_default();
+    if !rustc_wrapper.contains("sccache") {
+        println!(
+            "cargo::warning=Usage of sccache is highly recommended for fast builds. \
+            Consider setting up sccache."
+        );
+    }
+}
